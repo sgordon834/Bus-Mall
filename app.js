71510895a5f7ba72d3e2629e9ad3a88bd3e7d10f
sgordon834 <sgordon834@gmail.com>
2017-03-01 22:44:39 -0800

    touch up and clean up lines

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ function Product(name, path) {
   this.name = name;
   this.path = path;
   this.votes = 0;
-  // this.timesShown = 0;
+// this.timesShown = 0;
   productArray.push(this);
 }
+
 
 // Product.prototype.increment = function() {
 //   return timesShown += 1;
@@ -19,13 +20,12 @@ function Product(name, path) {
 for (var i = 0; i < productImages.length; i++) {
   var product = new Product(productImages[i], 'img/' + productImages[i] + '.jpg');
 }
-// console.log(productArray);
 
+function randomPictureIndex() {
+  return Math.floor(Math.random() * productImages.length);
+}
 
-// function randomPictureIndex() {
-//   return randomPic = Math.floor(Math.random() * productImages.length);
-// }
-
+//Tracks all votes, displays pictures, and events
 var tracker = {
   picOneEl: document.getElementById('picOne'),
   picTwoEl: document.getElementById('picTwo'),
@@ -38,53 +38,56 @@ var tracker = {
   pic3: null,
   clicks: 1,
 
-  randomPictureIndex: function() {
-    return Math.floor(Math.random() * productImages.length);
-  },
+//random pics display each time pic is selected
+  displayPics: function () {
+    this.pic1 = productArray[randomPictureIndex()];
+    this.pic2 = productArray[randomPictureIndex()];
+    this.pic3 = productArray[randomPictureIndex()];
 
-  displayPics: function() {
-    this.pic1 = productArray[this.randomPictureIndex()];
-    this.pic2 = productArray[this.randomPictureIndex()];
-    this.pic3 = productArray[this.randomPictureIndex()];
-
-    if (this.pic1 === this.pic2 || this.pic1 === this.pic3 || this.pic2 === this.pic3) {
+    while (this.pic1 === this.pic2 || this.pic1 === this.pic3 || this.pic2 === this.pic3) {
       this.displayPics();
     }
 
     this.picOneEl.src = this.pic1.path;
-    this.picOneEl.id = this.pic1.name;
     this.picTwoEl.src = this.pic2.path;
-    this.picTwoEl.id = this.pic2.name;
     this.picThreeEl.src = this.pic3.path;
+
+    this.picOneEl.id = this.pic1.name;
+    this.picTwoEl.id = this.pic2.name;
     this.picThreeEl.id = this.pic3.name;
+
   },
 
-  maxClicks: function() {
+//stops selcections after 15 clicks and allows results to be viewed
+  maxClicks: function () {
     console.log(this.clicks);
+    event.preventDefault();
     if (this.clicks > 14) {
       this.imageHolderEl.removeEventListener('click', this.handleClick);
-      this.results.addEventListener('click', function(e) {
-        e.preventDefault();
-        tracker.viewResults();
+      this.results.addEventListener('click', function (event) {
+
+tracker.viewResults();
       });
     }
 
   },
 
-  handleClick: function(e) {
+//Event handler for clicks, votes, and displays random pics
+  handleClick: function (event) {
     tracker.maxClicks();
     if (
-      e.target.id === tracker.pic1.name || e.target.id === tracker.pic2.name || e.target.id === tracker.pic3.name
+      event.target.id === tracker.pic1.name || event.target.id === tracker.pic2.name || event.target.id === tracker.pic3.name
     ) {
       tracker.clicks++;
-      tracker.countVotes(e.target.id);
+      tracker.countVotes(event.target.id);
       tracker.displayPics();
     }
   },
 
-  countVotes: function(elId) {
+//counts time pics are selected
+  countVotes: function (id) {
     for (var i = 0; i < productArray.length; i++) {
-      if (elId === productArray[i].name) {
+      if (id === productArray[i].name) {
         productArray[i].votes += 1;
         console.log(productArray[i]);
         return;
@@ -92,17 +95,21 @@ var tracker = {
     }
   },
 
-  viewResults: function() {
+//renders results
+  viewResults: function () {
     var ulEl = document.createElement('ul');
 
-    for (var i in productArray) {
+    for (var i = 0; i < productArray.length; i++) {
       var liEl = document.createElement('li');
       liEl.textContent = productArray[i].name + ': ' + productArray[i].votes;
       ulEl.appendChild(liEl);
     }
+
     this.viewResultsEl.appendChild(ulEl);
-  }
+  },
+
 };
 
+//event listener for tracker and call display pics function
 tracker.imageHolderEl.addEventListener('click', tracker.handleClick);
 tracker.displayPics();
