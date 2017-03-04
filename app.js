c55800bdc7f135c53195e2c3360fa310229afd0c
sgordon834 <sgordon834@gmail.com>
2017-03-04 01:59:25 -0800

    Adding color to chart

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ var productImages = ['banana', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthu
 
 
 var productArray = [];
-var counter = 0;
-var displayNames = [];
-var clickArray = [];
+var counter = [];
+// var displayNames = [];
 var results = document.getElementById('showMe');
 
 
@@ -14,21 +13,13 @@ function Product(name, path) {
   this.votes = 0;
 // this.timesShown = 0;
   productArray.push(this);
-  displayNames.push(this.name);
-  // chartData.data.datasets[0].data.push(this.votes);
-  // console.log(chartData);
-}
-
-// var ctx = document.getElementById("myChart");
-// var my = new Product(ctx, chartData);
-
-
-
+  // displayNames.push(this.name);
+};
 // Product.prototype.increment = function() {
 //   return timesShown += 1;
 // };
 
-for (var i = 0; i < productImages.length; i++) {
+for (var i in productImages) {
   var product = new Product(productImages[i], 'img/' + productImages[i] + '.jpg');
 }
 
@@ -36,93 +27,17 @@ function randomPictureIndex() {
   return Math.floor(Math.random() * productImages.length);
 }
 
-
-// var ctx = document.getElementById("myChart").getContext('2d');
-// var chartData = new Chart(ctx,  {
-//     type: 'bar',
-//     data: {
-//         labels: productImages,
-//         datasets: [{
-//             label: '# of Votes',
-//             data: data,
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-//                 'rgba(75, 192, 192, 0.2)',
-//                 'rgba(153, 102, 255, 0.2)',
-//                 'rgba(255, 159, 64, 0.2)'
-//             ],
-//             borderColor: [
-//                 'rgba(255,99,132,1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             borderWidth: 1
-//         }]
-//     }
-//
-// });
-
-// function handleResultsButton() {
-//   for (var j = 0; j < productImages.length; j++) {
-//   productArray.push(imageArray[j].name);
-//   clickArray.push(imageArray[j].clicks);
-//   }
-//   return;
-//   drawChart();
-// };
-
-
-// var ctx = document.getElementById('myChart').getContext('2d');
-//
-// // myChart.data.datasets[0].data => will get you the data array
-// var myChart = new Product(ctx, {
-//   type: 'bar',
-//   data: {
-//     labels: ['banana', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'tauntaun', 'unicorn', 'water-can', 'wine-glass'],// This will hold the name of each product image
-//     datasets: [{
-//       label: '# of Votes',
-//       data: [], // This will hold the votes for each product image
-//       backgroundColor: [
-//         'rgba(255, 99, 132, 0.2)',
-//         'rgba(54, 162, 235, 0.2)',
-//         'rgba(255, 206, 86, 0.2)',
-//         'rgba(75, 192, 192, 0.2)',
-//         'rgba(153, 102, 255, 0.2)',
-//         'rgba(255, 159, 64, 0.2)'
-//       ],
-//       borderColor: [
-//         'rgba(255,99,132,1)',
-//         'rgba(54, 162, 235, 1)',
-//         'rgba(255, 206, 86, 1)',
-//         'rgba(75, 192, 192, 1)',
-//         'rgba(153, 102, 255, 1)',
-//         'rgba(255, 159, 64, 1)'
-//       ],
-//       borderWidth: 1
-//     }]
-//   }
-// });
-
-
-
 //Tracks all votes, displays pictures, and events
 var tracker = {
   picOneEl: document.getElementById('picOne'),
   picTwoEl: document.getElementById('picTwo'),
   picThreeEl: document.getElementById('picThree'),
-  viewResultsEl: document.getElementById('aftermath'),
-  // results: document.getElementById('showMe'),
   imageHolderEl: document.getElementById('picHolder'),
   pic1: null,
   pic2: null,
   pic3: null,
   chartData: null,
-  clicks: 1,
+  totalClicks: 0,
 
 //random pics display each time pic is selected
   displayPics: function () {
@@ -146,86 +61,81 @@ var tracker = {
 
 //stops selcections after 15 clicks and allows results to be viewed
   maxClicks: function () {
-    console.log(this.clicks);
-    event.preventDefault();
-    if (this.clicks > 14) {
-      this.imageHolderEl.removeEventListener('click', this.handleClick);
-      this.results.addEventListener('click', function (event) {
-
-tracker.viewResults();
-      });
+    if(tracker.totalClicks > 14) {
+      tracker.imageHolderEl.removeEventListener('click', tracker.handleClick);
+      console.log('max clicks allowed');
+      storeData();
+      return;
     }
-
   },
 
 //Event handler for clicks, votes, and displays random pics
 
   handleClick: function (event) {
-    tracker.maxClicks();
+    // tracker.maxClicks();
     if (
       event.target.id === tracker.pic1.name || event.target.id === tracker.pic2.name || event.target.id === tracker.pic3.name
     ) {
-      tracker.clicks++;
-      tracker.countVotes(event.target.id);
+      console.log('Click picked!');
+      tracker.totalClicks++;
+      console.log(tracker.totalClicks);
+      tracker.countVotes();
       tracker.displayPics();
     }
+    tracker.maxClicks();
+
   },
 
 //counts time pics are selected
-  countVotes: function (id) {
-    for (var i = 0; i < productArray.length; i++) {
-      if (id === productArray[i].name) {
-        productArray[i].votes += 1;
-        console.log(productArray[i]);
-        return;
-      }
+  countVotes: function () {
+    if(event.target.id === tracker.pic1.name) {
+      tracker.pic1.votes++;
     }
-  },
-
-//renders results
-  viewResults: function () {
-    var ulEl = document.createElement('ul');
-
-    for (var i = 0; i < productArray.length; i++) {
-      var liEl = document.createElement('li');
-      liEl.textContent = productArray[i].name + ': ' + productArray[i].votes;
-      ulEl.appendChild(liEl);
+    if(event.target.id === tracker.pic2.name) {
+      tracker.pic2.votes++;
     }
+    if(event.target.id === tracker.pic3.name) {
+      tracker.pic3.votes++;
+    }
+    console.log('pic picked was ' + event.target.id);
+    tracker.displayPics();
+  }
+};
+// };
 
-    this.viewResultsEl.appendChild(ulEl);
-  },
-
+//move data into array
+var storeData = function() {
+  for(var i in productArray) {
+    counter.push(productArray[i].votes);
+  }
 };
 
+var dynamicColors = function() {
+    var r = Math.floor(Math.random() * 255);
+    var g = Math.floor(Math.random() * 255);
+    var b = Math.floor(Math.random() * 255);
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
 
-var handleResults = function() {
+
+tracker.imageHolderEl.addEventListener('click', tracker.handleClick);
+tracker.displayPics();
+
+var chartResults = function() {
   drawChart();
 };
-results.addEventListener('click', handleResults);
+
+results.addEventListener('click', chartResults);
 
 var data = {
   labels: productImages,
   datasets: [
     {
     label: 'CHARTS',
-    backgroundColor: [
-      'rgba(255, 99, 132, 0.2)',
-      'rgba(54, 162, 235, 0.2)',
-      'rgba(255, 206, 86, 0.2)',
-      'rgba(75, 192, 192, 0.2)',
-      'rgba(153, 102, 255, 0.2)',
-      'rgba(255, 159, 64, 0.2)'
-    ],
-    borderColor: [
-      'rgba(255,99,132,1)',
-      'rgba(54, 162, 235, 1)',
-      'rgba(255, 206, 86, 1)',
-      'rgba(75, 192, 192, 1)',
-      'rgba(153, 102, 255, 1)',
-      'rgba(255, 159, 64, 1)'
-    ],
+    backgroundColor: dynamicColors(),
+    borderColor: dynamicColors(),
     borderWidth: 1,
-    data: clickArray,
+    data: counter,
   }
   ]
 };
@@ -233,19 +143,8 @@ var data = {
 
 function drawChart() {
   var ctx = document.getElementById('myChart').getContext('2d');
-  busMallChart = new Chart(ctx,{
+  myChartData = new Chart(ctx,{
     type: 'bar',
     data: data,
   });
 };
-
-var ctx = document.getElementById('myChart').getContext('2d');
-
-// myChart.data.datasets[0].data => will get you the data array
-
-
-
-
-//event listener for tracker and call display pics function
-tracker.imageHolderEl.addEventListener('click', tracker.handleClick);
-tracker.displayPics();
