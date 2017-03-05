479ce870622b91dfe7a50edec7f33188fab3a31f
Sidney G <sgordon834@gmail.com>
2017-03-04 18:17:10 -0800

    Revert "Lab12 chart"

--- app.js
+++ app.js
@@ -2,49 +2,44 @@ var productImages = ['banana', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthu
 
 
 var productArray = [];
+var counter = 0;
 
 function Product(name, path) {
   this.name = name;
   this.path = path;
   this.votes = 0;
-  // this.timesShown = 0;
+// this.timesShown = 0;
   productArray.push(this);
-};
+}
+
+
 // Product.prototype.increment = function() {
 //   return timesShown += 1;
 // };
 
-for (var i in productImages) {
+for (var i = 0; i < productImages.length; i++) {
   var product = new Product(productImages[i], 'img/' + productImages[i] + '.jpg');
 }
-
-//JSON retieve data stored in productImages Array
-retrieveData();
 
 function randomPictureIndex() {
   return Math.floor(Math.random() * productImages.length);
 }
 
-//obj literals for use in functions/methods
+//Tracks all votes, displays pictures, and events
 var tracker = {
   picOneEl: document.getElementById('picOne'),
   picTwoEl: document.getElementById('picTwo'),
   picThreeEl: document.getElementById('picThree'),
-  imageHolderEl: document.getElementById('picHolder'),
-  viewResultsEl: document.getElementById('dispResults'),
+  viewResultsEl: document.getElementById('aftermath'),
   results: document.getElementById('showMe'),
-
+  imageHolderEl: document.getElementById('picHolder'),
   pic1: null,
   pic2: null,
   pic3: null,
-  chartData: null,
-  totalClicks: 0,
-  counter: [],
+  clicks: 1,
 
-
-
-  //random pics display each time pic is selected
-  displayPics: function() {
+//random pics display each time pic is selected
+  displayPics: function () {
     this.pic1 = productArray[randomPictureIndex()];
     this.pic2 = productArray[randomPictureIndex()];
     this.pic3 = productArray[randomPictureIndex()];
@@ -63,40 +58,48 @@ var tracker = {
 
   },
 
-  //stops selcections after 15 clicks and allows results to be viewed
-  maxClicks: function() {
-    if (tracker.totalClicks > 14) {
-      tracker.imageHolderEl.removeEventListener('click', tracker.handleClick);
-      console.log('max clicks allowed');
-      collectData();
-      tracker.viewResults();
-      storeData();
-      return;
+//stops selcections after 15 clicks and allows results to be viewed
+  maxClicks: function () {
+    console.log(this.clicks);
+    event.preventDefault();
+    if (this.clicks > 14) {
+      this.imageHolderEl.removeEventListener('click', this.handleClick);
+      this.results.addEventListener('click', function (event) {
+
+tracker.viewResults();
+      });
     }
+
   },
 
-  //Event handler for clicks, votes, and displays random pics
-
-  handleClick: function(event) {
-    // tracker.maxClicks();
+//Event handler for clicks, votes, and displays random pics
+  handleClick: function (event) {
+    tracker.maxClicks();
     if (
       event.target.id === tracker.pic1.name || event.target.id === tracker.pic2.name || event.target.id === tracker.pic3.name
     ) {
-      console.log('Click picked!');
-      tracker.totalClicks++;
-      console.log(tracker.totalClicks);
-      tracker.countVotes();
+      tracker.clicks++;
+      tracker.countVotes(event.target.id);
       tracker.displayPics();
-
     }
-    tracker.maxClicks();
-
   },
 
-  viewResults: function() {
+//counts time pics are selected
+  countVotes: function (id) {
+    for (var i = 0; i < productArray.length; i++) {
+      if (id === productArray[i].name) {
+        productArray[i].votes += 1;
+        console.log(productArray[i]);
+        return;
+      }
+    }
+  },
+
+//renders results
+  viewResults: function () {
     var ulEl = document.createElement('ul');
 
-    for (var i in productArray) {
+    for (var i = 0; i < productArray.length; i++) {
       var liEl = document.createElement('li');
       liEl.textContent = productArray[i].name + ': ' + productArray[i].votes;
       ulEl.appendChild(liEl);
@@ -105,77 +108,8 @@ var tracker = {
     this.viewResultsEl.appendChild(ulEl);
   },
 
-  //counts time pics are selected
-  countVotes: function() {
-    if (event.target.id === tracker.pic1.name) {
-      tracker.pic1.votes++;
-    }
-    if (event.target.id === tracker.pic2.name) {
-      tracker.pic2.votes++;
-    }
-    if (event.target.id === tracker.pic3.name) {
-      tracker.pic3.votes++;
-    }
-    console.log('pic picked was ' + event.target.id);
-    tracker.displayPics();
-  }
-};
-// };
-
-function collectData() {
-  for (var i = 0; i < productArray.length; i++) {
-    tracker.counter.push(productArray[i].votes);
-  }
 };
 
-//move data into array
-//set data/turn data into string
-function storeData() {
-  var stringProduct = JSON.stringify(productArray);
-  localStorage.setItem('stringProduct', stringProduct);
-  console.log(storeData);
-};
-
-//get data from stringify
-function retrieveData() {
-  var stringProduct = localStorage.getItem('stringProduct');
-  var parsedProducts = JSON.parse(stringProduct);
-  console.log(parsedProducts);
-}
-
-function dynamicColors() {
-  var r = Math.floor(Math.random() * 255);
-  var g = Math.floor(Math.random() * 255);
-  var b = Math.floor(Math.random() * 255);
-  return "rgb(" + r + "," + g + "," + b + ")";
-}
-
+//event listener for tracker and call display pics function
 tracker.imageHolderEl.addEventListener('click', tracker.handleClick);
-// tracker.viewResultsEl.addEventListener('click', chartResults);
-tracker.results.addEventListener('click', chartResults);
 tracker.displayPics();
-
-function chartResults() {
-  drawChart();
-};
-
-
-
-var data = {
-  labels: productImages,
-  datasets: [{
-    label: 'CHARTS',
-    backgroundColor: dynamicColors(),
-    borderColor: dynamicColors(),
-    borderWidth: 1,
-    data: tracker.counter,
-  }]
-};
-
-function drawChart() {
-  var ctx = document.getElementById('myChart').getContext('2d');
-  myChartData = new Chart(ctx, {
-    type: 'bar',
-    data: data,
-  });
-};
